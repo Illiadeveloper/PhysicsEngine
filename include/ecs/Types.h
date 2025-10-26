0e2ee2e27760a7450a75e163c99a1089722d02d4
Dovhal Illia <dovhal.illia21@gmail.com>
2025-10-26 13:39:23 +0100

    feat(ECS): add ECS architecture

---
+++ include/ecs/Types.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cstdint>
+#include <memory>
+#include <queue>
+#include <set>
+#include <typeinfo>
+#include <unordered_map>
+
+using Entity = std::uint32_t;
+const Entity MAX_ENTITIES = 5000;
+
+using ComponentType = std::uint8_t;
+const ComponentType MAX_COMPONENTS = 32;
+
+using Signature = std::bitset<MAX_COMPONENTS>;
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+#include "Coordinator.h"
+
 int main (int argc, char *argv[]) {
   std::cout << "GLM + GLAD + GLFW init" << std::endl;
   return 0;
