029ba7b0802bf5eaee3531ffe52a51e9da18d190
Dovhal Illia <dovhal.illia21@gmail.com>
2025-10-26 11:44:01 +0100

    init(file structure)

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+int main (int argc, char *argv[]) {
+  std::cout << "GLM + GLAD + GLFW init" << std::endl;
+  return 0;
+}
